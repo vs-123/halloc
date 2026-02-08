99229b3400a8ed12e7ac922b3074d9a777a8ce22
VS-123 <>
2026-02-08 14:09:40 +0000

    halloc: implement initialisation logic

--- src/main.c
+++ src/main.c
@@ -1,19 +1,44 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <assert.h>
 
-typedef struct block_t {
+#define POOL_SIZE 4096
+
+typedef struct block_t block_t;
+
+static uint8_t pool[POOL_SIZE];
+static block_t *free_list = NULL;
+
+struct block_t {
    size_t size;
    bool is_free;
    struct block_t *next;
-} block_t;
+};
 
 size_t align(size_t x) {
    return (x + 7) & ~7;
 }
 
-void halloc(size_t size) {
+void *halloc(size_t size) {
    size = align(size);
+   bool is_first_alloc = (free_list == NULL);
+
+   if (is_first_alloc) {
+      block_t *start_block = (block_t*)pool;
+      start_block->size = POOL_SIZE - sizeof(start_block);
+      start_block->is_free = true;
+      start_block->next = NULL;
+      free_list = start_block;
+   }
+
+   block_t *crntblk = free_list;
+   while (crntblk) {
+      if (crntblk->is_free && crntblk->size >= size) {
+         return (void*)(crntblk + 1);
+      }
+      crntblk = crntblk->next;
+   }
 }
 
 int main(void)
