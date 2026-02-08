76519a4b08cd405aa1eed167d9aa296f08e84b56
VS-123 <>
2026-02-08 13:58:57 +0000

    halloc: basic skeleton & add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# HALLOC
+
+A dead-simple heap allocator. More precisely, this is a general purpose explicit freelist allocator.
+
+## LICENSE
+
+This project is licensed under the GNU Affero General Public License version 3.0 or later.
+
+**NO WARRANTY PROVIDED**
+
+For full terms, see `LICENSE` file or visit **https://www.gnu.org/licenses/agpl-3.0.en.html**.
--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
+typedef struct block_t {
+   size_t size;
+   bool is_free;
+   struct block_t *next;
+} block_t;
+
+size_t align(size_t x) {
+   return (x + 7) & ~7;
+}
+
+void halloc(size_t size) {
+   size = align(size);
+}
+
+int main(void)
+{
    printf("hello, world!\n");
 
    return 0;
